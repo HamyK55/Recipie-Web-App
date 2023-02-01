64d86eb4fe3f1fb48b6eeb16385ed38a5024ac88
Humza <uptospace222@gmail.com>
2023-02-01 12:32:35 -0500

    added button on landing page

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -4,7 +4,22 @@ import {useNavigate } from 'react-router-dom';
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div class= 'landing-body'>
+      <div class='centered'>
+
+
+      <h1>The Cutting Board</h1>
+      <form>
+        <input class='search-input' type='search' placeholder='Find a Recipe'></input>
+        <button class='submit-btn' type='submit' >Search</button>
+
+
+      </form>
+
+      </div>
+
+
+    </div>
   )
 }
 
