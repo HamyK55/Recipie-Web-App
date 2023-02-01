76aa798e85b7843f5cb98e0c06024f5353360c75
Humza <uptospace222@gmail.com>
2023-02-01 12:21:23 -0500

    removed not found screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Landing from './screens/Landing';
 import Searched from './screens/Searched'
-import NotFound from './screens/NotFound';
 import {
   BrowserRouter as Router,
   Routes,
@@ -18,7 +17,6 @@ function App() {
         <Routes>
           <Route path='/' element={<Landing/>}/>
           <Route path='/recipe/:term' element={<Searched/>}/>
-          <Route path="*" element={<NotFound/>}/>
 
         </Routes>
 
