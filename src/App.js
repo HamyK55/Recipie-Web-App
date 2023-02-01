8c3ca07e362635c11ac623ae29a8195b1cb1cd98
Humza <uptospace222@gmail.com>
2023-02-01 11:54:42 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Landing from './screens/Landing';
+import Searched from './screens/Searched'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+function App() {
+  return (
+   
+  );
+}
+
+export default App;
---
+++ src/screens/Landing.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import '../App.css';
+import {useNavigate } from 'react-router-dom';
+
+function Landing() {
+  return (
+    <div>Landing</div>
+  )
+}
+
+export default Landing
