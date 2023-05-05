07594f6a00c06b7e22a498e33c5c5bd9e197d522
Humza <uptospace222@gmail.com>
2023-05-05 13:35:15 -0400

    added react for landing page

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -3,15 +3,30 @@ import '../App.css';
 import {useNavigate } from 'react-router-dom';
 
 function Landing() {
+  // js code 
+
+  const [recipe, setRecipe] = useState('');
+  const navigate = useNavigate();
+  
+  // function to navigate to page based on what "recipe" was submitted
+  const submitForm = (event) => {
+    event.preventDefault();
+    navigate('/recipe/' + recipe);
+  }
+
+  
+  
+  // All rendered out to screen
   return (
     <div class= 'landing-body'>
       <div class='centered'>
 
-
+      
       <h1>The Cutting Board</h1>
       <form>
-        <input class='search-input' type='search' placeholder='Find a Recipe'></input>
-        <button class='submit-btn' type='submit' >Search</button>
+        // onChange will trigger when an event(the info in the form is updated), info will be grabbed from the input form
+        <input onChange={(event) => {setRecipe(event.target.value)}} class='search-input' type='search' placeholder='Find a Recipe'></input>
+        <button onClick={submitForm} class='submit-btn' type='submit' >Search</button>
 
 
       </form>
