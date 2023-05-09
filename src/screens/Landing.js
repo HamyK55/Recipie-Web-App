9d1713bbf600aba8f3c581f0fe5af08753670d45
Humza <uptospace222@gmail.com>
2023-05-09 10:34:10 -0400

    removed comments to ensure no errors from tutorial

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,41 +1,29 @@
 import React, { useState } from 'react';
 import '../App.css';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Landing() {
-  // js code 
 
-  // recipe is a variable that can be changed with the setRecipe function (its made by the useState hook)
-  // recipe default value is ''
+
   const [recipe, setRecipe] = useState('');
   const navigate = useNavigate();
-  
-  // function to navigate(create a) to page based on what "recipe" was submitted
+
   const submitForm = (event) => {
     event.preventDefault();
     navigate('/recipe/' + recipe);
   }
 
-  
-  
-  // html rendered out to screen is all in return ()
-
-  // onChange will trigger when an event(the info in the form is updated), info will be grabbed from the input form
   return (
-    <div class= 'landing-body'>
+    <div class='landing-body'>
+
       <div class='centered'>
-      
-      <h1>The Cutting Board</h1>
-
+        <h1 class="landing-title">The Cutting Board</h1>
         <form>
-        
-          <input onChange={(event) => {setRecipe(event.target.value)}} class='search-input' type='search' placeholder='Find a Recipe'></input>
-          <button onClick={submitForm} class='submit-btn' type='submit' >Search</button>
-
-
+          <input class='search-input' type='search' placeholder='Find a Recipe' onChange={(event) => { setRecipe(event.target.value) }}></input>
+          <button class="submit-btn" type='submit' onClick={submitForm}>Search</button>
         </form>
-
       </div>
+
 
 
     </div>
