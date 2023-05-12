f4c8e2a01669503eaaaa59aeb00c845dec87fd75
Humza <uptospace222@gmail.com>
2023-05-12 11:23:13 -0400

    fixed, issue was white space on closing input tag

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -14,20 +14,24 @@ function Landing() {
   }
 
   return (
-    <div class='landing-body'>
+    
+    <div class='landing-body'> 
 
-      <div class='centered'>
+      <div class='centered'> 
+
         <h1 class="landing-title">The Cutting Board</h1>
+
         <form>
           <input class='search-input' type='search' placeholder='Find a Recipe' onChange={(event) => { setRecipe(event.target.value) }}></input>
           <button class="submit-btn" type='submit' onClick={submitForm}>Search</button>
         </form>
+
       </div>
 
-
-
     </div>
+
   )
+
 }
 
 export default Landing
